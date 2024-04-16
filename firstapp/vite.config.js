4f472bf80172428e25f92fd8547fb97b03726e14
Alisher <alisherxujanov@gmail.com>
2024-04-16 11:31:21 +0500

    installed sass

--- firstapp/vite.config.js
+++ firstapp/vite.config.js
@@ -1,3 +1,10 @@
+// HMR  =>  Hot Module Replacement
+//          This means that - when we save the file in the editor,
+//          then, the browser reloads the page automatically.
+// RU: Это означает, что - когда мы сохраняем файл в редакторе,
+//     тогда браузер автоматически перезагружает страницу.
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
