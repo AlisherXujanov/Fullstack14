328c35679354eb98fa9f73077658aa39932b10d7
Alisher <alisherxujanov@gmail.com>
2024-04-23 11:32:13 +0500

    created theme-switcher component

--- firstapp/src/components/Footer/ThemeSwitcher.jsx
+++ firstapp/src/components/Footer/ThemeSwitcher.jsx
@@ -1,7 +1,32 @@
-function ThemeSwitcher() {
-    return (
-        <div className="theme-switcher">
+import { useState } from 'react'
+import './themeSwitcher.scss'
 
+
+
+function ThemeSwitcher() {
+    const [theme, setTheme] = useState('light')
+
+
+    function switcher(e) {
+        if (theme === 'light') {
+            setTheme('dark')
+        } else {
+            setTheme('light')
+        }
+    }
+
+    return (
+        <div className={theme}>
+            <div className="theme-switcher">
+                <span className='icon'>☀️</span>
+
+                <div onClick={switcher} className="switcher" >
+                    <input type="checkbox" />
+                    <span className='circle'></span>
+                </div>
+
+                <span className='icon'>🌙</span>
+            </div>
         </div>
     );
 }
