ef57e18ec8bf65974f9faa3ca04cdb4a4b92fede
Alisher <alisherxujanov@gmail.com>
2024-04-26 11:03:01 +0500

    images

--- firstapp/src/components/About/index.jsx
+++ firstapp/src/components/About/index.jsx
@@ -1,6 +1,9 @@
+import "./style.scss"
+
 function About() {
     return (
         <div className='about-page'>
+            <p className="intro">NFT MARKETPLACE</p>
             <h1>About our Company NFT Distro</h1>
             <p>We are a cutting-edge company focused on the creation, promotion, and sale of non-fungible tokens (NFTs). Our marketplace offers a wide range of NFTs, from digital art and collectibles to gaming items and virtual real estate.</p>
         </div>
--- firstapp/src/components/AllComponents.jsx
+++ firstapp/src/components/AllComponents.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Layout from "./Layout"
 import LandingPage from "./LandingPage"
+import About from "./About"
 
 // eslint-disable-next-line no-unused-vars
 function AllComponents(props) {
@@ -11,7 +12,7 @@ function AllComponents(props) {
             <Routes location={location} key={location.pathname}>
                 <Route path='/' element={<Layout/>}>
                     <Route index element={<LandingPage/>} />
-                    <Route path="/about" element={"About page"} />
+                    <Route path="/about" element={<About/>} />
                     <Route path="/404" element={"404 Page Not Found"} />
                     {/* https://www.youtube.com/...
                     https://www.wikipedia.org/about */}
