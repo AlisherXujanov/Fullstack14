a737b315be84e854e0a075bb2c80851936656a58
Alisher <alisherxujanov@gmail.com>
2024-05-01 11:52:30 +0500

    dropdown

--- firstapp/src/components/CreateNFT/index.jsx
+++ firstapp/src/components/CreateNFT/index.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable no-unused-vars */
 import Salin5 from "../../assets/imgs/Salin5.jpg"
+import Sali from "../../assets/icons/Sali.png"
 import "./style.scss"
 import AddImage from '../../assets/icons/addImage.png'
+import bottomVector from '../../assets/icons/bottom.png'
+import { useState } from "react"
 
 function CreateNFT(props) {
+    const [option, setOption] = useState("ExBoot Collection")
+    const [showOption, setShowOption] = useState(false)
+
+    function activateOption(e) {
+        setOption(e.target.textContent)
+    }
+    function activateShowOption(e) {
+        setShowOption(!showOption)
+    }
+
     return (
         <div className="nft-form-wrapper">
             <h1>Create new NFT</h1>
@@ -30,15 +43,24 @@ function CreateNFT(props) {
                     <div className="form-control">
                         <label htmlFor="collections">Collections</label>
 
-                        <div className="options-wrapper">
+                        <div className="options-wrapper" onClick={activateShowOption}
+                            style={showOption ? {boxShadow:'-2px -2px 16px lightgray'} : {boxShadow:'0 0'}}
+                        >
                             <div className="option-image">
-                                <img src={Salin5} alt="Collection-image" />
+                                <img src={Sali} alt="Collection-image" />
                             </div>
-                            <select>
-                                <option value="1">ExBoot Collection</option>
-                                <option value="2">ExBoot Collection 2</option>
-                                <option value="3">ExBoot Collection 3</option>
-                            </select>
+                            <div className="option-value">
+                                {option}
+                                <img 
+                                    style={showOption ? {transform:'rotate(180deg)'} : {transform:'rotate(0)'}}
+                                    src={bottomVector} alt="vector-image" 
+                                />
+                            </div>
+                            <div style={showOption ? {display:'block'} : {display:'none'}} className="drp-content">
+                                <div onClick={activateOption}>ExBoot Collection</div>
+                                <div onClick={activateOption}>ExBoot Collection 2</div>
+                                <div onClick={activateOption}>ExBoot Collection 3</div>
+                            </div>
                         </div>
                     </div>
                 </form>
