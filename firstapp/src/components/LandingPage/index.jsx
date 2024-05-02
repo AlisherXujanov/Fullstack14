8031f8e6bbbfb346fe8b3d7ab97df9adb05d99ad
Alisher <alisherxujanov@gmail.com>
2024-05-02 12:20:59 +0500

    wave effect

--- firstapp/src/components/LandingPage/index.jsx
+++ firstapp/src/components/LandingPage/index.jsx
@@ -2,16 +2,17 @@ import Ownership from "./Ownership"
 import TrendingArt from "./TrendingArt";
 import Collections from "./Collections";
 import Leaderboard from "./Leaderboard"
+import "./style.scss";
 
 
 function LandingPage() {
     return (
-        <>
+        <main className="landing-page-wrapper">
             <Ownership />
             <TrendingArt/>
             <Collections/>
             <Leaderboard />
-        </>
+        </main>
     );
 }
 
