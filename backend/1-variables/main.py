ad9805a90df2789adde775d7d5b918c730ac8142
AlisherKhujanov <alisherxujanov163@gmail.com>
2024-05-29 15:39:53 +0500

    backend first-lesson

--- backend/1-variables/main.py
+++ backend/1-variables/main.py
@@ -14,7 +14,7 @@
 # # print(isinstance(z, int))  # True
 # print(type(z) == int)    # True
 # ----------------------------------------------------------
-x = "2"  # 2
+# x = "2"  # 2
 ## int(x)  ==  parseInt(x)
 
 # print("Before: ", type(x))
@@ -50,3 +50,47 @@ x = "2"  # 2
 # print(13 // 3) #  4
 # print(14 // 4) #  3
 # ----------------------------------------------------------
+# print(2 > 3)     # False
+# print(3 <= 5)    # True
+# print(3 == 3)    # True
+# print(9/3 == 3)  # True
+# print(2**2 != 5) # True
+# print(9//4 >= 2) # True
+# print(15%6 < 5)  # True
+# ----------------------------------------------------------
+# ||   ==    or
+# &&   ==    and
+# !    ==    not
+
+# print(2 == 2 or 3 == 4 or 5 == 6 or 7 == 10)  # True
+# print(2 == 5 or 3 == 4 or 5 == 6 or 7 == 10)  # False
+
+# print(2 == 2 and 3 == 3 and 4 == 4 and 5 == 5)  # True
+# print(2 == 2 and 3 == 3 and 4 == 4 and 5 == 6)  # False
+
+# print(not 2 == 2)  # False
+# print(not 3 == 2)  # True
+# ----------------------------------------------------------
+# Банковый баланс
+bank_balance = 0
+
+# Депозит
+deposit = 10000
+
+# Процентная ставка
+interestRate = 0.1
+
+# Задание:
+# Рассчитываем доход по депозиту
+# bank_balance = bank_balance + deposit
+bank_balance += deposit
+print("Our balance: $", bank_balance)
+
+# когда мы хотим узнать 17% от  $67
+# 1.  (67 / 100) * 17   => 17% от 67
+# 2.  67 * 0.17         => 17% от 67
+
+income_after_1_year = bank_balance * interestRate
+print("After 1 year: $", bank_balance + income_after_1_year)
+
+print("Monthly: $", income_after_1_year/12)
