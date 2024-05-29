bbb47cfef0df3f4571f209afa2087d2890cef02a
AlisherKhujanov <alisherxujanov163@gmail.com>
2024-05-29 15:12:14 +0500

    created backend part of APP

---
+++ backend/main.py
@@ -0,0 +1,52 @@
+# sep  =>  separator   =>  разделитель
+# end  =>  end of line =>  конец строки
+# print("Hello", "world!", "Again", "it", "is ...", sep="+")
+# ----------------------------------------------------------
+# x = "qwe"
+# z = 5
+
+# print(type(x)) # <class 'str'>
+# print(type(z)) # <class 'int'>
+
+# # print(isinstance(x, str))  # True
+# print(type(x) == str)    # True
+
+# # print(isinstance(z, int))  # True
+# print(type(z) == int)    # True
+# ----------------------------------------------------------
+x = "2"  # 2
+## int(x)  ==  parseInt(x)
+
+# print("Before: ", type(x))
+# x = int(x)
+# print("After: ", type(x))
+# print("-----------------------------------")
+## str(x)  ==  String(x)
+# x = 10
+# print("Before: ", type(x))
+# x = str(x)
+# print("After: ", type(x))
+# ----------------------------------------------------------
+# NUMBER = 12
+# print(NUMBER)
+# NUMBER = 15
+# print(NUMBER)
+# ----------------------------------------------------------
+# %  ->  Modulus
+# number = 5
+# print(number % 2)
+# number = 11
+# print(number % 3)
+# ----------------------------------------------------------
+# **  =>  power  =>  степень
+# number = 3
+# print(number ** 3)
+# number = 2
+# print(number ** 4)
+# ----------------------------------------------------------
+# //  =>  floor division  =>  целочисленное деление
+# int(3.123)  #  3
+# print(11 // 2) #  5
+# print(13 // 3) #  4
+# print(14 // 4) #  3
+# ----------------------------------------------------------
