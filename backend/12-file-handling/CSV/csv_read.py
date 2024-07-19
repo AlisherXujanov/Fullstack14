4d850a40138636960e893b6f712c7a0dcf3caa53
Alisher <alisherxujanov163@gmail.com>
2024-07-19 15:00:00 +0500

    full14 update

---
+++ backend/12-file-handling/CSV/csv_read.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('./users.csv', mode='r') as file:
+    file_reader = csv.reader(file, delimiter=',', quotechar='"')
+
+    for row in file:
+        print(row)
+    file.close()
---
+++ backend/12-file-handling/HW.py
@@ -0,0 +1,41 @@
+# 1. Create and write to a file with names and read the file and print the names and the number of times they appear in the file.
+# RU: Создайте и запишите в файл имена, прочитайте файл и напечатайте имена и количество раз, которое они встречаются в файле.
+def read_names():
+    ...
+
+
+def create_names(names):
+    ...
+
+
+names = [
+    "Oliver", "George", "Harry", "Jack", "Jacob", "Noah",
+    "Charlie", "Muhammad", "Thomas", "Oscar", "William",
+    "James", "Leo", "Alfie", "Henry", "Archie", "Ethan",
+    "Charlie", "Muhammad", "Thomas", "Oscar", "William",
+    "Joseph", "Freddie", "Samuel", "Alexander", "Logan"
+]
+
+create_names(names)
+read_names()
+
+# ======================================================================================================================
+# ======================================================================================================================
+# 2. Create a file with numbers and read the file and print the sum of all numbers.
+# RU: Создайте файл с числами, прочитайте файл и напечатайте сумму всех чисел.
+
+
+def read_numbers_and_sum():
+    ...
+
+
+def create_numbers():
+    ...
+
+
+create_numbers()
+read_numbers_and_sum()
+
+
+# ======================================================================================================================
+# ======================================================================================================================
