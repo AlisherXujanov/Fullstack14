696cdc56dbb18c1ec0c3e9e5561c5cebddec6746
Alisher <alisherxujanov163@gmail.com>
2024-09-11 11:31:21 +0500

    Shoxsanam

---
+++ Shoxsanam/todo/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_page(request):
+    context = {
+        "title": "Welcome to Django Todo App",
+    }
+    return render(request, "home.html", context)
