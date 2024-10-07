00c20ffd1aa3b2cfef2d01c294beee27c4ecded7
Alisher <alisherxujanov163@gmail.com>
2024-10-07 11:00:49 +0500

    created signals

--- Shoxsanam/todo/todo/settings.py
+++ Shoxsanam/todo/todo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users',
     'django_bootstrap5',
     'django_cleanup.apps.CleanupConfig',
 ]
